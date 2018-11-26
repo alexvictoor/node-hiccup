47255cdbaa470ec7d3170c627683371e2aaff7a3
Alexandre Victoor <alexandre.victoor@fluo.com>
2018-11-26 22:19:34 +0100

    clean up client code and reduce recording frequency

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,4 @@
 import {
-    Event,
     RecordHiccupEvent,
     StartHiccupRecorderEvent,
     StopHiccupRecorderEvent
@@ -8,45 +7,69 @@ import { ChildProcess } from "child_process";
   
   
 export class HiccupRecorder {
-    private recorderLoop: any;
-  
-    constructor(
-      private worker: ChildProcess,
-      private controlIdleWorker: ChildProcess,
-      private tag = "HICCUP",
-      private resolutionMs = 50,
-      private reportingIntervalMs = 60000,
-    ) {}
-  
-    start() {
-      const startEvent: StartHiccupRecorderEvent = {
-        type: "start",
-        resolutionMs: this.resolutionMs,
-        reportingIntervalMs: this.reportingIntervalMs,
-        tag: this.tag
-      };
+  private recorderLoop: any;
 
-      this.worker.send(startEvent);
-      this.controlIdleWorker.send(startEvent);
-      let timeBeforeMeasurement = process.hrtime();
-      this.recorderLoop = setInterval(() => {
-        const delta = process.hrtime(timeBeforeMeasurement);
-        const recordEvent: RecordHiccupEvent = {
-          type: "record",
-          deltaTimeMicroSec: Math.floor(delta[0] * 1e6 + delta[1] / 1e3)
-        };
-        this.worker.send(recordEvent);
-        timeBeforeMeasurement = process.hrtime();
-      }, this.resolutionMs);
+  constructor(
+    private worker: ChildProcess,
+    private controlIdleWorker: ChildProcess | null,
+    private tag = "HICCUP",
+    private idleTag = "CONTROL_IDLE",
+    private resolutionMs = 100,
+    private reportingIntervalMs = 30000,
+  ) {}
+
+  start() {
+    this.startWorker();
+    this.startControlIdleWorker();
+    this.startRecorderLoop();
+  }
+
+  private startWorker() {
+    const startEvent: StartHiccupRecorderEvent = {
+      type: "start",
+      resolutionMs: this.resolutionMs,
+      reportingIntervalMs: this.reportingIntervalMs,
+      tag: this.tag
+    };
+    this.worker.send(startEvent);
+  }
+
+  private startControlIdleWorker() {
+    if (!this.controlIdleWorker) {
+      // no control idle needed
+      return;
     }
-  
-    stop() {
-      clearInterval(this.recorderLoop);
-      const stopEvent: StopHiccupRecorderEvent = {
-        type: "stop"
+    const startEvent: StartHiccupRecorderEvent = {
+      type: "start",
+      resolutionMs: this.resolutionMs,
+      reportingIntervalMs: this.reportingIntervalMs,
+      tag: this.idleTag
+    };
+    this.controlIdleWorker.send(startEvent);
+  }
+
+  private startRecorderLoop() {
+    let timeBeforeMeasurement = process.hrtime();
+    this.recorderLoop = setInterval(() => {
+      const delta = process.hrtime(timeBeforeMeasurement);
+      const recordEvent: RecordHiccupEvent = {
+        type: "record",
+        deltaTimeMicroSec: Math.floor(delta[0] * 1e6 + delta[1] / 1e3)
       };
-      this.worker.send(stopEvent);
+      this.worker.send(recordEvent);
+      timeBeforeMeasurement = process.hrtime();
+    }, this.resolutionMs);
+  }
+  
+  stop() {
+    clearInterval(this.recorderLoop);
+    const stopEvent: StopHiccupRecorderEvent = {
+      type: "stop"
+    };
+    this.worker.send(stopEvent);
+    if (this.controlIdleWorker) {
       this.controlIdleWorker.send(stopEvent);
     }
   }
+}
   
