db4bbca8a10ebb42d0f7c6836c9fe1d5bb25ecb9
Alexandre Victoor <alexandre.victoor@fluo.com>
2019-03-07 15:49:00 +0100

    npx cli with relative path

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Like jHiccup, node-hiccup runs a loop and keeps track of the delay between two t
 You can use node-hiccup without touching any line of code. This can be done leveraging on npx. The only thing you need to do is to replace 'node' by 'npx node-hiccup' in the command line used to bootstrap your application:
 
 ```
-  npx node-hiccup /ABSOLUTE_PATH/server.js  // assuming that server.js is the bootstrap script for your app
+  npx node-hiccup server.js  // assuming that server.js is the bootstrap script for your app
 
 ```
 
--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
+import { isAbsolute, sep } from 'path';
 import monitor from './index';
 
 const scriptParam = process.argv[2];
 console.log(`Starting to instrument with node-hiccup script ${scriptParam}`);
+let appPath;
+if (isAbsolute(scriptParam)) {
+    appPath = scriptParam;
+} else {
+    appPath = process.cwd() + sep + scriptParam;
+}
 try {
-    require.resolve(scriptParam);
+    require.resolve(appPath);
 } catch (err) {
     console.error('Could not find javascript module', scriptParam);
     throw err;
 }
 monitor();
-require(scriptParam);
+require(appPath);
